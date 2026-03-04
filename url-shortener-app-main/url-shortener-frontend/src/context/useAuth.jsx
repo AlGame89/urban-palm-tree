235709a0e4dabd32e1a3d3e9f868be26d06893b7
AlGame89 <169075696+AlGame89@users.noreply.github.com>
2026-03-04 22:31:13 +0300

    Delete url-shortener-app-main directory

--- url-shortener-app-main/url-shortener-frontend/src/context/useAuth.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from './AuthContext';
-
-// custom hook for accessing authentication context
-export const useAuth = () => {
-    return useContext(AuthContext);
-};
--- url-shortener-app-main/url-shortener-frontend/src/pages/HomePage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ShortenerForm from '../components/ShortenerForm';
-
-export default function HomePage() {
-    return <ShortenerForm />;
-}
